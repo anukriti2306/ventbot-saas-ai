93cd6856220bff45b94ebefa0a08e5e6ad1d33ed
anukriti_23 <anukriti2306@gmail.com>
2025-08-04 18:44:24 +0530

    finished initial tests for backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,3 +1,8 @@
 import express from "express";
+//app variable
 const app = express();
+//define middleware
+app.use(express.json());
+//connections and listeners
+app.listen(5000, () => console.log("Server Open"));
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,9 @@
 import express from "express";
-
+//app variable
 const app = express();
+
+//define middleware
+app.use(express.json());
+
+//connections and listeners
+app.listen(5000, ()=>console.log("Server Open")); 
