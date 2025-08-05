e6231b35ad80e84cd489970bab0ca15e03df08ca
anukriti_23 <anukriti2306@gmail.com>
2025-08-05 20:24:35 +0530

    added in the signup routes, validation chain functions

---
+++ backend/src/utils/validators.ts
@@ -0,0 +1,27 @@
+import { error } from "console";
+import { NextFunction, Response, Request} from "express";
+import { body, ValidationChain, validationResult } from "express-validator";
+
+const validate = (validations: ValidationChain[]) =>{
+    return async (req: Request, res: Response, next: NextFunction)=>{
+        for(let validation of validations){
+            const errors = await validation.run(req);
+            if(!errors.isEmpty()){
+                break;
+            }
+        }
+        const errors = validationResult(req);
+        if(errors.isEmpty()){
+            return next();
+        }
+        return res.status(422).json({errors: errors.array()});
+    };
+}; 
+
+const signupValidator = [
+    body("name").notEmpty().withMessage("Name is required."),
+    body("email").trim().isEmail().withMessage("Email is required."),
+    body("password").trim().isLength({min:6}).withMessage("Password should contain atleast 6 chars."),
+];
+
+export {validate, signupValidator};
